3da90c39df937fecc64dace10db1f31b00d2bb8f
MichaelD! <michael@dragonspark.us>
2016-02-20 13:28:53 -0500

    Prepping for Azure Key Vault.

---
+++ Framework/DragonSpark.Windows.Testing/Security/KeyVaultApplicationFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DragonSpark.Windows.Security;
+using Xunit;
+
+namespace DragonSpark.Windows.Testing.Security
+{
+	public class KeyVaultApplicationFactoryTests
+	{
+		[Theory, DragonSpark.Testing.Framework.Setup.AutoData]
+		public void Create( KeyVaultApplicationFactory sut, Uri location, Guid id )
+		{
+			var input = $"cn={location.DnsSafeHost},OU={id}";
+
+			var created = sut.Create( input );
+			Assert.Equal( id, created.Id );
+			Assert.Equal( $"https://{location.Host}/", created.Location.ToString() );
+		}
+	}
+}
---
+++ Framework/DragonSpark.Windows/Security/KeyVaultApplicationFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CPI.DirectoryServices;
+using DragonSpark.Activation.FactoryModel;
+using DragonSpark.Extensions;
+using PostSharp.Patterns.Contracts;
+
+namespace DragonSpark.Windows.Security
+{
+	public class KeyVaultApplication
+	{
+		public KeyVaultApplication( [Required]Guid id, [Required]Uri location )
+		{
+			Id = id;
+			Location = location;
+		}
+
+		public Guid Id { get; set; }
+		public Uri Location { get; set; }
+	}
+
+	public class KeyVaultApplicationFactory : FactoryBase<string, KeyVaultApplication>
+	{
+		public static KeyVaultApplicationFactory Instance { get; } = new KeyVaultApplicationFactory();
+
+		protected override KeyVaultApplication CreateItem( string parameter )
+		{
+			var name = new DN( parameter );
+			var components = name.RDNs.SelectMany( rdn => rdn.Components ).ToArray();
+			var id = Get( components, "OU" );
+			var location = Get( components, "CN" );
+			var result = new KeyVaultApplication( new Guid( id ), new Uri( $"{Uri.UriSchemeHttps}://{location}" ) );
+			return result;
+		}
+
+		static string Get( IEnumerable<RDNComponent> components, string name ) => components.WithFirst( component => string.Equals( component.ComponentType, name, StringComparison.CurrentCultureIgnoreCase ), component => component.ComponentValue );
+	}
+}
